9693df4099fc5d5314a5677e3f8dba49b6244eef
Simon <omojide.o.j@gmail.com>
2025-05-05 08:56:56 +0100

    Improve UI navbar and weather display

--- modules/validation.js
+++ modules/validation.js
@@ -3,15 +3,3 @@ export const sanitizerInput = (rawText) => {
   if (typeof rawText !== 'string') return '';
   return rawText.trim().replace(/[^a-zA-Z ]/g, '');
 }
-
-
-// Time converter
-export const timeToLocalTime = (data) => {
-  if (typeof data !== 'number') return '';
-  return new Date(data * 1000).toLocaleTimeString()
-}
-
-// Message sanitizer
-export const addMessage =  (message) => {
-  return message;
-}
